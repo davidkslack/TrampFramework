b656d9158cd9f8217e96aecc64d0e953e9f3a763
Dave <davidkslack@yahoo.co.uk>
2014-05-03 17:59:36 +0100

    Added template styles and scripts

--- app/Templates/Default/page.html.php
+++ app/Templates/Default/page.html.php
@@ -9,113 +9,19 @@
 	<title><?php print $this->headerTitle ?></title>
 
 	<!-- Bootstrap core CSS -->
-	<link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css">
-
-	<style>
-		/*
- * Base structure
- */
-
-		/* Move down content because we have a fixed navbar that is 50px tall */
-		body {
-			padding-top: 50px;
-		}
-
-
-		/*
-		 * Global add-ons
-		 */
-
-		.sub-header {
-			padding-bottom: 10px;
-			border-bottom: 1px solid #eee;
-		}
-
-
-		/*
-		 * Sidebar
-		 */
-
-		/* Hide for mobile, show later */
-		.sidebar {
-			display: none;
-		}
-		@media (min-width: 768px) {
-			.sidebar {
-				position: fixed;
-				top: 51px;
-				bottom: 0;
-				left: 0;
-				z-index: 1000;
-				display: block;
-				padding: 20px;
-				overflow-x: hidden;
-				overflow-y: auto; /* Scrollable contents if viewport is shorter than content. */
-				background-color: #f5f5f5;
-				border-right: 1px solid #eee;
-			}
-		}
-
-		/* Sidebar navigation */
-		.nav-sidebar {
-			margin-right: -21px; /* 20px padding + 1px border */
-			margin-bottom: 20px;
-			margin-left: -20px;
-		}
-		.nav-sidebar > li > a {
-			padding-right: 20px;
-			padding-left: 20px;
-		}
-		.nav-sidebar > .active > a {
-			color: #fff;
-			background-color: #428bca;
-		}
-
-
-		/*
-		 * Main content
-		 */
-
-		.main {
-			padding: 20px;
-		}
-		@media (min-width: 768px) {
-			.main {
-				padding-right: 40px;
-				padding-left: 40px;
-			}
-		}
-		.main .page-header {
-			margin-top: 0;
-		}
-
-
-		/*
-		 * Placeholder dashboard ideas
-		 */
-
-		.placeholders {
-			margin-bottom: 30px;
-			text-align: center;
-		}
-		.placeholders h4 {
-			margin-bottom: 0;
-		}
-		.placeholder {
-			margin-bottom: 20px;
-		}
-		.placeholder img {
-			display: inline-block;
-			border-radius: 50%;
-		}
-
-	</style>
+	<link rel="stylesheet" href="/app/Templates/Default/css/bootstrap.min.css">
+	<link rel="stylesheet" href="/app/Templates/Default/css/bootstrap-theme.min.css">
+	<link rel="stylesheet" href="/app/Templates/Default/css/style.css">
 
 	<!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
 	<!--[if lt IE 9]>
 	<script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
 	<script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
 	<![endif]-->
+
+	<script src="/app/Templates/Default/js/jquery-1.11.0.min.js"></script>
+	<script src="/app/Templates/Default/js/bootstrap.min.js"></script>
+	<script src="/app/Templates/Default/js/script.js"></script>
 </head>
 
 <body>
@@ -163,7 +69,5 @@
 	</div>
 </div>
 
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
-<script src="//netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
 </body>
 </html>
